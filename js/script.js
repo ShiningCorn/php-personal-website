b9c1c957f18777d0b0a02059b5ed8f876e24c525
Minh Ngo <developerminh@gmail.com>
2023-12-13 17:22:28 -0600

    Update UI

---
+++ js/script.js
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////
+// Mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);   
+})
+
+///////////////////////////////////////////////////////////
+// Dark mode
+
+var darkmodeicon = document.getElementById("icon-dark-mode");
+darkmodeicon.onclick = function(){
+  document.body.classList.toggle("dark-mode");
+  if(document.body.classList.contains("dark-mode")) {
+    darkmodeicon.setAttribute("name", "sunny-outline");
+  }else {
+    darkmodeicon.setAttribute("name", "moon-outline");
+  }
+}
