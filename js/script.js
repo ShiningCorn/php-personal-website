a58cc6268c7bc9e4231ff0d6012801f06480e6d9
Minh Ngo <minh@minhngo.us>
2024-10-24 00:01:51 -0500

    fix link bug

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
     if (href === "#")
+      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: "smooth",
@@ -27,6 +27,7 @@ allLinks.forEach(function (link) {
 
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
@@ -40,20 +41,20 @@ allLinks.forEach(function (link) {
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);   
-})
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 ///////////////////////////////////////////////////////////
 // Dark mode
 
 var darkmodeicon = document.getElementById("icon-dark-mode");
-darkmodeicon.onclick = function(){
+darkmodeicon.onclick = function () {
   document.body.classList.toggle("dark-mode");
-  if(document.body.classList.contains("dark-mode")) {
+  if (document.body.classList.contains("dark-mode")) {
     darkmodeicon.setAttribute("name", "sunny-outline");
-  }else {
+  } else {
     darkmodeicon.setAttribute("name", "moon-outline");
   }
-}
+};
